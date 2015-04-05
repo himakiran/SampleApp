e135338b90a4a4ef165d855ebfa09cc837f664c1
chundi himakiran kumar <himakiran@gmail.com>
2015-04-05 13:56:42 +0530

    renamed-the original SampleApp folder as Old-SampleApp. copied all orginal files into this folder. Now all the recently developed files from circle-of-friends have been brought into the SampleApp folder.

---
+++ circle-of-friends/Old-SampleApp/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#">
+<head>
+  <meta charset="utf-8" />
+  <title>SampleApp</title>
+  <link href='https://fonts.googleapis.com/css?family=Exo:700,700italic|Average' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="_/css/mystyle-v1.css"> <!-- IMPORTANT Browsers and facebook cache css files hence to see
+  changes in your css during development rename css file with every new modification like mystyle-v1.css mystyle-v2.css -->
+  <script type="text/javascript" src="_/js/jquery-2.1.0.js"></script>
+  <script type="text/javascript" src="_/js/myscript.js"></script>
+</head>
+<body>
+  <div id="fb-root"></div>
+  <div id="content">
+    <div id="main" >
+      <div id="fb-root"></div>
+      <div id="mymessage"></div>
+      <div id="welcome"></div>
+      <img id="img1" src="images/self.jpg" alt="Chundi Himakiran Kumar" /> 
+      <div id="msg"></div>
+      <div id="bio"></div>
+      <div id="bday"></div>
+      <div id="email"></div>
+      <div id="uname"></div>     
+    </div><!--main-->
+    <div id="sidebar" >
+      <div id="friendslist"></div>
+      <div id="videogroup"></div><!--videogroup -->
+    </div><!--sidebar -->         
+    
+  </div><!--content -->
+  <script type="text/javascript" src="http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json&callback=populateVideos"></script>
+</body>
+</html>
